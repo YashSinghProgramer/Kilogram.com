0a4700ac349a7b89cdba99413e8c26ff6bd786d5
YashSinghProgramer <yashsinghprogramer@gmail.com>
2026-05-26 20:20:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Login from './Login/login.jsx'
 import Mainpage from './main/mainapp.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {  Route, Routes } from 'react-router-dom'
 function App() {
   return (
 
