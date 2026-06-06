ab83e5cbcc736b9a5bd20ee3c879f75411ee80d8
YashSinghProgramer <yashsinghprogramer@gmail.com>
2026-06-06 10:18:12 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,9 @@
 const app = require("./app/app.js");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+	res.send("Server is Live mubark ho");
+});
 app.listen(PORT, () => {
 	console.log(`Server is runnning on PORT: ${PORT}`);
 });
