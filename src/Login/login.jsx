8fc69923a96c150a4fd02ae2a0d80d2a7305b7e8
YashSinghProgramer <yashsinghprogramer@gmail.com>
2026-06-06 10:39:34 +0530

    add media

--- src/Login/login.jsx
+++ src/Login/login.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import style from './logi.module.css'
-import Leftside from '../left side/left.jsx'
-import Rightside from '../right side/right.jsx'
+import React from "react";
+import style from "./logi.module.css";
+import Leftside from "../left side/left.jsx";
+import Rightside from "../right side/right.jsx";
 function login() {
-  return (
-     <div className={style.Con}>
-        <Leftside/>
-        <Rightside/>
-    </div>
-  )
+	return (
+		<div className={style.Con}>
+			<Leftside className={style.lefft} />
+			<Rightside className={style.right} />
+		</div>
+	);
 }
 
-export default login
+export default login;
